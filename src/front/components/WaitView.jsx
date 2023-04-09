1e4d62d6b44b10e7282b8d53aea3e5bb0d9ca2d0
alejandropmz <alejandropacheco2497@gmail.com>
2023-04-08 20:26:40 -0500

    gif para la pantalla de espera

--- src/front/components/WaitView.jsx
+++ src/front/components/WaitView.jsx
@@ -4,10 +4,11 @@ export const WaitView = ({ type }) => {
   return (
     <div id="load-container" className="d-flex justify-content-center align-items-center">
       <img
-        src="https://wallup.net/wp-content/uploads/2015/12/274119-Star_Wars-stormtrooper-Darth_Vader-mask-splitting-minimalism.jpg"
+        src="https://media1.giphy.com/media/k57vTvlTaN1SLDu1vO/giphy.gif?cid=ecf05e47yrgmy3jc9vqxge8r0j50gx82o69sj1bqx53dchaz&rid=giphy.gif&ct=g"
         alt=""
       />
       <h1>STARSWARS {type} IS LOADING...</h1>
     </div>
   );
 };
+
